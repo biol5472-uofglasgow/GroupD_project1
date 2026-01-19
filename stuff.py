2d3f72f65ff7cb9d0a6a8a1bd07bfe0ca3910de1
George Collins <2619195C@student.gla.ac.uk>
2026-01-19 13:41:03 +0000

    adding misc code to stuff.py

---
+++ stuff.py
@@ -0,0 +1,28 @@
+#loading in a fasta file: 
+
+#loading in the kinases file 
+seq_dict = {}
+with open('/home1/bioinfo-10/lab1BIOL4292/lab4/Toxo_Ser_Thr_kinase.fasta') as kinase: 
+    for line in kinase: 
+        line = line.rstrip()
+        if line.startswith(">"):
+            toxo_gene_ID = line[1:]
+        else: 
+            seq_dict[toxo_gene_ID] = line
+
+
+#function to calcule GC content: 
+def calc_gc_content(seq):
+    c_count = seq.count('C')
+    g_count = seq.count('G')
+    gc_content = ((c_count + g_count) / len(seq)) * 100
+    #print(f'this is the GC content : {gc_content}%')
+    return round(gc_content)
+
+#length of sequence: 
+def total_bases(seq): 
+    length = len(seq)
+    return length 
+
+#n content
+def n_content(Seq):  
