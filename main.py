14c0d833e856d304b13eef272d5e9cba73c468ce
Annalise L.Brown <alarivierebr@gmail.com>
2026-01-22 11:23:20 +0000

    Added filename checking and some logging

--- main.py
+++ main.py
@@ -17,18 +17,36 @@ def main(args):
     fh.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s - %(message)s'))
     logger.addHandler(fh)
 
-    #if fasta ->
+    
     if not os.path.isdir(args.folder_path):
+        logger.error(f'Input path {args.output_path} could not be found. Please check your input and try again\n')
         raise SystemExit(1) #validating that input folder path exists, add error checking and logging, need to make these if statements and system exit if false
 
-    if not os.path.isdir(args.output_path):
+    if not os.path.isdir(args.output_path):#
+        logger.error(f'Output path {args.output_path} could not be found. Please check your input and try again\n')
         raise SystemExit(1) #validating that output folder path exists, add error checking logging etc later
 
-
-
+    
     for filename in os.listdir(args.folder_path): #gets all the files inside the folder specified at the CLI
         logger.info(f'{filename}') #logs the file names to the log, I made a test folder to test that this works
         #make error checking to verify the folder paths exists and are valid
 
+        if filename.endswith() not in ('fasta', 'fasta.gz', '.fastq', 'fastq.gz'):
+            logger.info(f'The file {filename} could not be read') #log the file name of the non fasta/fastq file
+            pass #skip over
 
-    #if fastq ->
+        if filename.endswith('.fasta', '.fasta.gz') == True:
+            #Import the class and function that parses fasta and execute here
+            pass #this will be replaced with the class.function() for fasta parsing
+        if filename.endswith('.fastq', '.fastq.gz') == True:
+            pass #this will be replaced with the class.function() for fastq parsing
+
+
+
+    #put parsers in classes in another file, and then in for loop here, determine if file is fasta or fastq and execute the appropriate parser functions
+    #
+
+ 
+
+
+    
