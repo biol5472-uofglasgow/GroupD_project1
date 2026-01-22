9b998397adffe5eabe1ea542d350e0dc79f9269c
Annalise L.Brown <alarivierebr@gmail.com>
2026-01-22 01:53:55 +0000

    Setting up initial start code layout

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-from Bio.Seq import SeqIO
-from Bio.Seq import Seq, MutableSeq
-import argparse
 import logging
+import os
 
-'''
-Setting up argparser to take in arguments at the terminal
-'''
-parser = argparse.ArgumentParser(description=' \n', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('fasta_file', help='Input the fasta file name you want to parse')
-parser.add_argument('fastaq_file', help='Input the vcf file name you want to parse\n')
-parser.add_argument('', help= 'Input the fasta file name you want to parse')
-parser.add_argument('output_prefix', help='Input the name for output')
-args = parser.parse_args()
 
-'''
-Logging setup
-Logs information, errors, etc in a file
-'''
+#Where it is decided whether the file is fasta or fastq and set down the appropriate pathway for parsing
+def main(args):
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(f'{args.output_prefix}.log')
-fh.setLevel(logging.INFO)
-fh.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s - %(message)s'))
-logger.addHandler(fh)
+    '''
+    Logging setup
+    Logs information, errors, etc in a file
+    '''
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(f'{args.log_name}.log')
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s - %(message)s'))
+    logger.addHandler(fh)
+
+    #if fasta ->
+    if not os.path.isdir(args.folder_path):
+        raise SystemExit(1) #validating that input folder path exists, add error checking and logging, need to make these if statements and system exit if false
+
+    if not os.path.isdir(args.output_path):
+        raise SystemExit(1) #validating that output folder path exists, add error checking logging etc later
+
+
+
+    for filename in os.listdir(args.folder_path): #gets all the files inside the folder specified at the CLI
+        logger.info(f'{filename}') #logs the file names to the log, I made a test folder to test that this works
+        #make error checking to verify the folder paths exists and are valid
+
+
+    #if fastq ->
