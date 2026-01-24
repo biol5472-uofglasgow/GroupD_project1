b71ef4af8a3fc3b345b66594abdc54717c87f7fd
Annalise L.Brown <alarivierebr@gmail.com>
2026-01-24 01:49:33 +0000

    Added output writing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import logging
 import os
 import pyfastx
-from parsing_files import FastQ_Typing
+from parsing_files import FASTQ, FASTQ_Qual, FASTA, write_fasta_tsv, write_fastq_tsv
+from typing import Any
 
 
 
-#Where it is decided whether the file is fasta or fastq and set down the appropriate pathway for parsing
 def main(args):
 
     '''
@@ -13,14 +13,16 @@ def main(args):
     Logs information, errors, etc in a file
     '''
 
-    logger = logging.getLogger()
+    logger = logging.getLogger(__name__)
+    logger.propagate = False
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler(f'{args.log_name}.log')
-    fh.setLevel(logging.INFO)
-    fh.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s - %(message)s'))
-    logger.addHandler(fh)
 
-    from parsing_files import FASTQ, FASTQ_Qual
+    if not logger.handlers:
+        fh = logging.FileHandler(f'{args.log_name}.log')
+        fh.setLevel(logging.INFO)
+        fh.setFormatter(logging.Formatter('%(levelname)s - %(asctime)s - %(message)s'))
+        logger.addHandler(fh)
+
 
     '''
     Setting accepted filetypes and folder path variables
@@ -39,74 +41,135 @@ def main(args):
     Validating folder paths
 
     '''
-             
+                    
     if not os.path.isdir(folder_path):
-        logger.error(f'Input path {args.output_path} could not be found. Please check your input and try again\n')
+        logger.error(f'Input path {folder_path} could not be found. Please check your input and try again\n')
         raise SystemExit(1) #validating that input folder path exists, add error checking and logging, need to make these if statements and system exit if false
 
     if not os.path.isdir(output_path):#
-        logger.error(f'Output path {args.output_path} could not be found. Please check your input and try again\n')
+        logger.error(f'Output path {output_path} could not be found. Please check your input and try again\n')
         raise SystemExit(1) #validating that output folder path exists, add error checking logging etc later
 
 
-    def fasta_reader(fastafile: str):
+    '''
+    Defining output actions per file
+    
+    '''
 
+    for filename in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, filename)
         try:
-            fasta_file = pyfastx.Fasta(os.path.join(folder_path, filename))
-            logger.info(f'{filename}') #logs the file names to the log, I made a test folder to test that this works
-            #make error checking to verify the folder paths exists and are valid
+            if filename.endswith(fasta_filetypes):
+                fa = FASTA(full_path)
+                logger.info(f'{filename}') #logs the file names to the log, I made a test folder to test that this works
+                #make error checking to verify the folder paths exists and are valid
+                logger.info(f'The file {filename} will work')
+                #this will be replaced with the class.function() for fasta parsing
+                fa = FASTA(full_path)
+                records = fa.records
 
-            #Import the class and function that parses fasta and execute here
-            logger.info(f'The file {filename} will work')
-            print(f'Fasta file {filename} will be parsed here')
-            #this will be replaced with the class.function() for fasta parsing
+                out_file = os.path.join(
+                    output_path,
+                    f"{os.path.splitext(filename)[0]}_records.tsv"
+                )
+
+                records: list[dict[str, Any]]
+                # records = [record]
+
+        
+                write_fasta_tsv(records, out_file)
+
+            elif filename.endswith(fastq_filetypes):
+                fq= FASTQ(full_path)
+                fqq= FASTQ_Qual(full_path)
+                
+                summary_record = {
+                    "total_bases": fq.total_bases,
+                    "gc_fraction": fq.gc_fraction,
+                    "avg_len": fq.avg_len,
+                    "phred_score": fq.phred_score,
+                    "A_count": fq.A_count,
+                    "C_count": fq.C_count,
+                    "G_count": fq.G_count,
+                    "T_count": fq.T_count,
+                    "N_count": fq.N_count,
+                    # "mean_qual": fqq.read_info["mean_qual"],
+                    # "q30_fraction": fqq.read_info["q30_fraction"],
+                }
+                records: list[dict[str, Any]]
+                records = [summary_record]
+
+                out_file = os.path.join(
+                    output_path,
+                    f"{os.path.splitext(filename)[0]}.tsv"
+                )
+
+                write_fastq_tsv(records, out_file)
+                
             
         except RuntimeError as e:
             logger.info(f'The file {filename} could not be read. Error: {e}') #log the error
             raise SystemExit(1) # NOTE, wondering if you guys think SystemExit here is appropriate? Do we want to stop running if a file is broken? or just skip over it?
 
 
-    def fastq_reader(fastqfile: str):
-        #     if filename.endswith(fastq_filetypes):
-        try:
-            fq = pyfastx.Fastq(os.path.join(folder_path, fastqfile))
-            logger.info(f'{fastqfile}') #logs the file names to the log, I made a test folder to test that this works
-            #make error checking to verify the folder paths exists and are valid
+'''
 
-            #Import the class and function that parses fasta and execute here
-            logger.info(f'The file {fastqfile} will be parsed')
-            #this will be replaced with the class.function() for fasta parsing
+NOTE to myself for tomorrow's workflow:
 
-            #this is where we will loop in the output write function once its ready!
-            print(FASTQ.total_bases(fq))
-            print(FASTQ.gc_fraction(fq))
-            print(FASTQ.N_cont(fq))
-            print(FASTQ.avg_len(fq))
-            print(FASTQ.phred_score(fq))
-            print(FASTQ_Qual.read_info(fq))
+Each file does output information now, but missing some columns, check on columns for each output.
+Loop mean_qual and q30_fraction in to fastq output and make sure it's as expected
+'''
+#         def fastq_reader(filename: str):
+#             #     if filename.endswith(fastq_filetypes):
+#             try:
+#                 fa = pyfastx.Fastq(os.path.join(folder_path, filename))
+#                 logger.info(f'{fq}') #logs the file names to the log, I made a test folder to test that this works
+#                 #make error checking to verify the folder paths exists and are valid
 
-        except RuntimeError as e:
-            logger.info(f'The file {fastqfile} could not be read. Error: {e}') #log the error
-            pass
+#                 #Import the class and function that parses fasta and execute here
+#                 logger.info(f'The file {fq} will be parsed')
+#                 #this will be replaced with the class.function() for fasta parsing
 
+#                 #this is where we will loop in the output write function once its ready!
+#                 print(fq.total_bases())
+#                 print(fq.gc_fraction())
+#                 print(fq.N_cont())
+#                 print(fq.avg_len())
+#                 print(fq.phred_score())
+#                 print(fq.read_info())
 
-
-    for filename in os.listdir(folder_path):
-            if filename.endswith(fasta_filetypes):
-                fasta_reader(fastafile=filename)
-                print(f'Fasta file {filename} has been parsed')
-                continue
-        
-        
-            if filename.endswith(fastq_filetypes):
-                fastq_reader(fastqfile=filename)
-                print(f'Fastq file {filename} has been parsed')
-                continue
-            
+#             except RuntimeError as e:
+#                 logger.info(f'The file {fq} could not be read. Error: {e}') #log the error
+#                 pass
 
 
 
- 
+# for filename in os.listdir(folder_path):
+#     if filename.endswith(fasta_filetypes):
+#         fa = pyfastx.Fasta(os.path.join(folder_path, filename))
+#         with open(f'{filename}_output.tsv') as output:
+#             #name of new file, how to grab the name of the original file
+#             output.write(fasta_reader(fa))
+#             print(f'Fasta file {fa} has been parsed')
+#             continue
 
 
-    
+#     if filename.endswith(fastq_filetypes):
+#         fq = pyfastx.Fastq(os.path.join(folder_path, filename))
+#         fastq_reader(fq)
+#         print(f'Fastq file {filename} has been parsed')
+#         continue
+
+           
+    # def FASTA_write_tsv(self, fasta_read_count, samp_id, fasta_total, fasta_gc, n_count, fasta_av_len):
+    #         fa = pyfastx.Fastq(os.path.join(folder_path, filename))
+    #         with open(f'{filename}_output.tsv') as output:
+    #             output.write('Sample_ID\tn_seqs_of_reads\ttotal_bases\tmean_len\tgc_fraction\tn_fraction\n')
+    #             output.write(f"{samp_id}\t{fasta_read_count}\t\t{fasta_total}\tt{fasta_av_len}\t\t{fasta_gc}\t\t{n_count}\n")
+                            
+
+    # def FASTQ_write_tsv(self, meanq_data, qual30_data):
+    #     with open('results.tsv', 'w') as output_table:
+    #         output_table.write(f'Mean_Quality\tqual_over_30\n')
+    #     #Output mean_qual and qual over 30
+
