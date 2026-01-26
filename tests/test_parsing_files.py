aa7e47d849a89f8023958f5d4a8d0f19c843c321
Amrita Trehan <amritatrehan@Amritas-MacBook-Pro.local>
2026-01-26 18:53:32 +0000

    added more tests

--- tests/test_parsing_files.py
+++ tests/test_parsing_files.py
@@ -1,6 +1,20 @@
 import pytest
 from parsing_files import FASTQ, FASTQ_Qual, FASTA
 
+##FASTA 
+def test_avg_len():
+    fa = FASTA('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/contigs.fasta')
+    assert fa.avg_len == 68
+
+def test_summary_row():
+    fa = FASTA('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/contigs.fasta')
+    assert fa.summary == [12] ## set the value here - ask annalise 
+
+def test_write_fasta_tsv():
+    with pytest.raises(TypeError):
+        fa=FASTA.records('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/contigs.fasta')
+
+### FASTQ
 def test_total_bases():
     fq = FASTQ("/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq")
     assert fq.total_bases == 48
@@ -8,6 +22,26 @@ def test_total_bases():
 def test_gc_content():
     fq =FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
     assert fq.gc_fraction == 54.16666793823242
+
+def test_base_composition():
+    fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
+    assert fq.base_composition == {'A':13, 'C':14, 'G':12, 'T':9, 'N': 0}
+
+def test_A_count():
+    fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
+    assert fq.A_count == 13
+
+def test_C_count():
+    fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
+    assert fq.C_count == 14
+
+def test_G_count():
+    fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
+    assert fq.G_count == 12
+
+def test_T_count():
+    fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
+    assert fq.T_count == 9
 
 def test_N_count():
     fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
@@ -24,9 +58,9 @@ def test_phred_score():
 
 def test_fastq_qual_sum():
     fq = FASTQ_Qual('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
-    fq.read_info(fq)
-    assert fq.read_count == 1920
-
+    #fq.read_info(fq)
+    assert fq.read_info == (2, 68, 1, 68)
+"""
 def test_fastq_qual_bases():
     fqq = FASTQ_Qual()
     fq = FASTQ('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/sampleA.fastq')
@@ -45,7 +79,7 @@ def test_mean_quality():
     fqq.read_info(fq)
     qsum = fqq.qual_sum
     qbases = fqq.qual_bases
-    assert FASTQ_Qual.mean_quality(qsum, qbases) == 40.0
+    assert FASTQ_Qual.mean_quality(qsum, qbases) == 40.0 
 
 def test_q30_frac():
     fqq = FASTQ_Qual()
@@ -54,13 +88,14 @@ def test_q30_frac():
     q30 = fqq.q30_bases
     qbases = fqq.qual_bases
     assert FASTQ_Qual.mean_quality(q30, qbases) == 1.0
-
-
-def test_avg_len():
-    fa = FASTA('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/contigs.fasta')
-    assert fa.avg_len == 68
-
-
+     
 def test_read_fasta():
     fa = FASTA('/Users/amritatrehan/Desktop/Software_proj/GroupD_project1/contigs.fasta')
-    assert fa.read_fasta == (2, ['chr1', 'chr2'], [68, 68], [50.0, 50.0], [4, 4]) 
+    assert fa.read_fasta == (2, ['chr1', 'chr2'], [68, 68], [50.0, 50.0], [4, 4]       """
+
+
+
+
+
+
+#assert fq.N_count == {'A':13, 'C':14, 'G':12, 'T':9, 'N': 0}
