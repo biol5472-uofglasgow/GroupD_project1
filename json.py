26cbb944a605d0c711663917dd69f29f038eaed0
George Collins <2619195C@student.gla.ac.uk>
2026-01-28 10:31:09 +0000

    changing from uv to hatchling as uv not working/installing

--- json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open ("results.tsv", "r") as f:
-    data = json.load(f)
-print(data)
-print(type(data))    
-
-#can include tool version?
--- src/groupD_tool/__init__.py
+++ src/groupD_tool/__init__.py
@@ -1,3 +1,3 @@
-from . import main as main 
+"""from . import main as main 
 from . import parsing_files as parsing_files
-from . import run as run
+from . import run as run"""
